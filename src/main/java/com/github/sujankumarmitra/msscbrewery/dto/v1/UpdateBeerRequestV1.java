3cff796f0e1126ebb694f803632912fc7cd1c952
skmitra <mitrakumarsujan@gmail.com>
2021-01-04 18:41:12 +0530

    added Javadoc

--- src/main/java/com/github/sujankumarmitra/msscbrewery/dto/v1/UpdateBeerRequestV1.java
+++ src/main/java/com/github/sujankumarmitra/msscbrewery/dto/v1/UpdateBeerRequestV1.java
@@ -2,6 +2,12 @@ package com.github.sujankumarmitra.msscbrewery.dto.v1;
 
 import com.github.sujankumarmitra.msscbrewery.model.v1.BeerV1;
 
+/**
+ * Request Body POJO of {@link com.github.sujankumarmitra.msscbrewery.resource.v1.BeerResourceV1#updateBeer(String, UpdateBeerRequestV1)} )}
+ *
+ * @author skmitra
+ * @version 1.0
+ */
 public class UpdateBeerRequestV1 implements BeerV1 {
 
     private String id;
